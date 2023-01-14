e4af884ef43a77247f1811175a4165b2649c5b53
mdYeasin-cmd <yeasin200011@gmail.com>
2023-01-14 23:07:55 +0600

    Children Props type added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Button';
 import DataFetch from './components/DataFetch';
+import Post from './components/Post';
 
 
 
 function App() {
   return (
     <div className="App">
-      <h1>User Management</h1>
-      <DataFetch status="error" />
+      <h1>Style Props</h1>
     </div>
   );
 }
---
+++ src/components/Post.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './Card';
+
+const Post = () => {
+    return (
+        <Card>
+            <h2>Post title 1</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure consectetur eveniet labore doloribus dignissimos rem pariatur tempora voluptates, illo quidem.</p>
+        </Card>
+    );
+};
+
+export default Post;
